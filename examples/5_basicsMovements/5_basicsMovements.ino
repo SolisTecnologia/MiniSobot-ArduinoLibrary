ea2e6c3c239d5f0eb7e00ce81280957b4a98bb28
Solis Tecnologia - Root <67121369+SolisTecnologia@users.noreply.github.com>
2024-04-04 15:55:29 -0300

    att: basicsMovements change parameters

--- examples/5_basicsMovements/5_basicsMovements.ino
+++ examples/5_basicsMovements/5_basicsMovements.ino
@@ -1,20 +1,20 @@
 /****************************************************************************
 MiniSobot Library
 Created By   : Rodrigo L. de Carvalho
-Version      : 1.0
+Version      : 1.1
 Company      : Solis Tecnologia
 
 basicsMovements.ino :
     This sketch demonstrates the use of the all fuction to drive MiniSobot.
-    drive_forward(speed, duration)
-    drive_backward(speed, duration)
+    drive_forward(speed)
+    drive_backward(speed)
     drive_curve_left(speed)
     drive_curve_right(speed)
     drive_curve_differential_left(speed, percent)
     drive_curve_differential_right(speed, percent)
     drive_break()
 
-    All speed parameters has a default 255 value. 
+    All speed parameters has a default 150 value. 
     and use drive_break() function whenever changing the direction of the motor
 ****************************************************************************/
 
@@ -34,12 +34,12 @@ void setup(){
 // The loop function runs over and over again forever
 void loop (){
 
-    robot.drive_forward(200,0); // Forward
+    robot.drive_forward(200); // Forward
     delay(2000); // Wait a two seconds
     robot.drive_break(); // Stop the motors
     delay(2000); // Wait a two seconds
 
-    robot.drive_backward(200,0); // Backward
+    robot.drive_backward(200); // Backward
     delay(2000); // Wait a two seconds
     robot.drive_break(); // Stop the motors
     delay(2000); // Wait a two seconds
